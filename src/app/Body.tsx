2e2f1fd990b660d9dbce812697218fd6627fdb9b
Flourish Olukotun <flourisholukotun@gmail.com>
2025-04-08 17:15:09 +0100

    Update content

--- src/app/Body.tsx
+++ src/app/Body.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { motion } from "framer-motion";
